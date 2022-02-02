30207c02bad97402ecbe8ac5095a1b3bf877fe74
Bullcaos <18469085+Bullcaos@users.noreply.github.com>
2022-02-02 18:21:41 +0100

    Initial Commit

---
+++ 2022-pizza-test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2022-pizza-test
+//
+//  Created by Bullcaos on 2/2/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
